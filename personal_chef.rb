be93eabf6309f5d8389aae7837f76125f992a43b
Jonah <jonahkmoses@gmail.com>
2013-06-18 18:13:15 -0700

    Objects, Attributes, and Methods

--- personal_chef.rb
+++ personal_chef.rb
@@ -1,8 +1,15 @@
 class PersonalChef
-	def make_toast
-		puts "Making your toast!"
+	def make_toast(color)
+		puts "Making your toast, #{color}, sir"
 	end
-	
 
+	def make_milkshake(flavor)
+		puts "Making your #{flavor} milkshake!"
+	end
+end
 
+class Butler
+	def open_door(door)
+		puts "Opening #{door} Door, Sir!"
+	end
 end
