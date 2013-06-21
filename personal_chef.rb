00568b61bd399b6d440ce85e30c03b239150a623
Jonah <jonahkmoses@gmail.com>
2013-06-21 14:55:10 -0700

    added conditional if/elsif/else statements for boiling water and countdown timer

--- personal_chef.rb
+++ personal_chef.rb
@@ -41,6 +41,28 @@ class PersonalChef
 		end
 	end
 
+	def water_status(minutes)
+		if minutes < 7
+			puts "The water is not boiling yet."
+		elsif minutes == 7
+			puts "It's just barely boiling"
+		elsif minutes == 8
+			puts "It's boiling!"
+		else
+			puts "Hot! Hot! Hot!"
+		end
+		return self
+	end
+
+	def countdown(counter)
+		while counter > 0
+			puts "The counter is #{counter}"
+			counter = counter-1
+		end
+		return self
+	end
+
+
 end
 
 
